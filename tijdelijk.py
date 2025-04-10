32e2dcdaa38bb3a25210bcc05970852fddee98de
Martijn Plevier <tinus523@gmail.com>
2025-04-10 12:47:00 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,34 +1,40 @@
-smaken = {
-    "aardbei" : 3,
-    "vanille" : 4,
-    "chocolade" : 5
-}
+from helper import decoreer
 
-aanbieding = smaken["aardbei"]*0.8
-#print(aanbieding)
+def print_aanbieding():
+    smaken = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5
+    }
 
-reclame_tekst=f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
-#print(reclame_tekst)
+    aanbieding = smaken["aardbei"]*0.8
+    #print(aanbieding)
 
-reclame_tekst2 = reclame_tekst[:63]
-#print(reclame_tekst2)
+    reclame_tekst=f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+    #print(reclame_tekst)
 
-reclame_tekst3 = reclame_tekst2.upper()
-#print(reclame_tekst3)
+    reclame_tekst2 = reclame_tekst[:63]
+    #print(reclame_tekst2)
 
-reclame_tekst4 = reclame_tekst3.split()
-#print(reclame_tekst4)
+    reclame_tekst3 = reclame_tekst2.upper()
+    #print(reclame_tekst3)
+
+    reclame_tekst4 = reclame_tekst3.split()
+    #print(reclame_tekst4)
 
 
-list = reclame_tekst4
-#for el in list:
-#    el = el.lower()
-#    print(el)
+    list = reclame_tekst4
+    #for el in list:
+    #    el = el.lower()
+    #    print(el)
 
-for el in list:
-    if len(el) >= 5:
-        # Als het element 5 of meer tekens heeft, zet het om naar hoofdletters
-        print(el.upper())
-    else:
-        # Als het element 4 of minder tekens heeft, zet het om naar kleine letters
-        print(el.lower())
+    for el in list:
+        if len(el) >= 5:
+            # Als het element 5 of meer tekens heeft, zet het om naar hoofdletters
+            print(el.upper())
+        else:
+            # Als het element 4 of minder tekens heeft, zet het om naar kleine letters
+            print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()
